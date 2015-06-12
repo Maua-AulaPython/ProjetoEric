96568f831e2f03e0e7255add3d3caf356948491a
matheusnno <matheusnno@gmail.com>
2015-06-12 12:43:41 -0300

    projeto1

---
+++ model.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+from mauaserver import db
+
+
+class Estufa(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    localizacao = db.Column(db.String(40))
+    medidas = db.relationship('Medida')
+
+
+class Medida(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    estufa_id = db.Column(db.Integer, db.ForeignKey('estufa.id')) 
+    umidade = db.Column(db.Float)
+    temperatura = db.Column(db.Float)
